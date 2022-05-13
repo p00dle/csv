af10010a7076481d14515b7edb6e9e62cba41aa2
Krzysztof Siuda <kksiuda@gmail.com>
2022-05-13 10:59:18 +0100

    Removed dist dir

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export type { CsvColumn, CsvColumns, CsvColumnType, CsvParams } from './types';
-export { parseCsv, parseCsvFromStream, createParseCsvStream, ParseCsvTransformStream } from './parse';
-export { stringifyCsv, stringifyCsvFromStream, createStringifyCsvStream, StringifyCsvTransformStream, } from './stringify';
-//# sourceMappingURL=index.d.ts.map
--- dist/simple-date.d.ts
+++
@@ -1,8 +0,0 @@
-import type { DateClass, DateParams } from './types';
-export declare class SimpleDate implements DateClass {
-    private formatType;
-    constructor(params?: DateParams);
-    parse(str: string): number;
-    stringify(n: number): string;
-}
-//# sourceMappingURL=simple-date.d.ts.map
